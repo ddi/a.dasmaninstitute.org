17e6d5ffafeba5571c6a5739d92bd8c155a2e9b5
Mohammad Al Qersh <m@qersh.com>
2025-06-14 19:01:13 +0300

    DA-24 models created

---
+++ app/Http/Controllers/BmeBrandController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BmeBrand;
+use Illuminate\Http\Request;
+
+class BmeBrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BmeBrand $bmeBrand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BmeBrand $bmeBrand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BmeBrand $bmeBrand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BmeBrand $bmeBrand)
+    {
+        //
+    }
+}
